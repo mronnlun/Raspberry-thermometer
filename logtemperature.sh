143ab0aee00c7367a7df2e1c4ebd823888eb3290
Mathias <mathias.ronnlund@gmail.com>
2015-11-20 20:42:17 +0200

    Update logtemperature.sh

--- logtemperature.sh
+++ logtemperature.sh
@@ -8,10 +8,10 @@ regex='Humidity = ([0-9]+\.[0-9]+) % Temperature = ([0-9]+\.[0-9]+)'
 [[ $output =~ $regex ]]
 
 hum="${BASH_REMATCH[1]}"
-echo hum $hum
+#echo hum $hum
 
 temperature="${BASH_REMATCH[2]}"
-echo temperature $temperature
+#echo temperature $temperature
 
 currdate=$(date +"%Y-%m-%dT%H:%M:%S") 
 echo $currdate $temperature $hum >> ~/persistenttemperaturelog.txt
@@ -19,10 +19,10 @@ echo $currdate $temperature $hum >> ~/persistenttemperaturelog.txt
 key=$(<$HOME/thingspeak-key.txt)
 
 url="https://api.thingspeak.com/update?key=$key&field1=$temperature&field2=$hum"
-echo $url
+#echo $url
 
 result="$(curl -s $url > /home/pi/thingspeak_curl_silent.txt)"
-echo result $result
+#echo result $result
 
 
 
