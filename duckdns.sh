927fd36d9b316e68b2488274ea8af006d2929ffe
Mathias <mathias.ronnlund@gmail.com>
2015-12-02 21:30:22 +0200

    Script to register ip to duckdns

--- duckdns.sh
+++ duckdns.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
 
+duckname=$(<$HOME/duck-name.txt)
+ducktoken=$(<$HOME/duck-token.txt)
+
+url = "https://www.duckdns.org/update?domains=$dns&token=$ducktoken&ip="
+echo url=$url | curl -k -o ~/duck.log -K -
