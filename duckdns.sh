94bee0fef596b8b9ad02e152752a97230a2d8e76
Mathias <mathias.ronnlund@gmail.com>
2015-12-02 22:12:48 +0200

    Changed curl

--- duckdns.sh
+++ duckdns.sh
@@ -4,4 +4,4 @@ duckname=$(<$HOME/duck-name.txt)
 ducktoken=$(<$HOME/duck-token.txt)
 
 url = "https://www.duckdns.org/update?domains=$dns&token=$ducktoken&ip="
-echo url=$url | curl -k -o ~/duck.log -K -
+result="$(curl -s $url > $HOME/duck.log)"
