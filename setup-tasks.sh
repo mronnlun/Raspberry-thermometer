f7d184d3f3fb9b670eed092c534b96f6f8dd9db5
Mathias <mathias.ronnlund@gmail.com>
2015-12-02 21:31:48 +0200

    Added duckdns

--- setup-tasks.sh
+++ setup-tasks.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 cat <(crontab -l) <(echo "*/3 * * * * $HOME/Raspberry-thermometer/logtemperature.sh") | crontab -
+cat <(crontab -l) <(echo "*/15 * * * * $HOME/Raspberry-thermometer/duckdns.sh") | crontab -
