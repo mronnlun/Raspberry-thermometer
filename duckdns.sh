df0156c79647b8110299dffc0c1caa51c9e7941c
Mathias <mathias.ronnlund@gmail.com>
2015-12-10 09:07:05 +0200

    Added verbose option

--- duckdns.sh
+++ duckdns.sh
@@ -3,5 +3,5 @@
 duckname=$(<$HOME/duck-name.txt)
 ducktoken=$(<$HOME/duck-token.txt)
 
-url="https://www.duckdns.org/update?domains=$duckname&token=$ducktoken&ip="
+url="https://www.duckdns.org/update?domains=$duckname&token=$ducktoken&ip=&verbose"
 result="$(curl -s $url > $HOME/duck.log)"
