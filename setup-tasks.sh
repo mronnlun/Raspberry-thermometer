4f1eb965dbedc884e49a50ef8c421f4201b81874
Mathias <mathias.ronnlund@gmail.com>
2015-12-24 01:18:24 +0200

    Remove sudo

--- setup-tasks.sh
+++ setup-tasks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat <(sudo crontab -l) <(echo "*/5 * * * * $HOME/Raspberry-thermometer/logtemperature.sh") | crontab -
-cat <(sudo crontab -l) <(echo "*/5 * * * * $HOME/Raspberry-thermometer/duckdns.sh") | crontab -
+cat <(crontab -l) <(echo "*/5 * * * * $HOME/Raspberry-thermometer/logtemperature.sh") | crontab -
+cat <(crontab -l) <(echo "*/5 * * * * $HOME/Raspberry-thermometer/duckdns.sh") | crontab -
