8d6bd4715a44f3359e4c8dde81996035cabd1d9e
Mathias <mathias.ronnlund@gmail.com>
2015-12-24 00:56:46 +0200

    Echo url

--- duckdns.sh
+++ duckdns.sh
@@ -4,4 +4,5 @@ duckname=$(<$HOME/duck-name.txt)
 ducktoken=$(<$HOME/duck-token.txt)
 
 url="https://www.duckdns.org/update?domains=$duckname&token=$ducktoken&ip=&verbose"
-result="$(curl -s $url > $HOME/duck.log)"
+echo $url > $HOME/duck.log
+result="$(curl -s $url >> $HOME/duck.log)"
